d957f5cd8440c0268c00a2281f1ea54a18177fd9
Corey Russ <corey.j.russ@gmail.com>
2018-05-29 00:16:23 -0700

    Update scaffolding and prepare deployment files

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
+const logger = require('morgan');
+
 
 module.exports = {
-  init(){
+
+  init(app, express){
+
+    app.use(logger('dev'));
 
   }
 };
